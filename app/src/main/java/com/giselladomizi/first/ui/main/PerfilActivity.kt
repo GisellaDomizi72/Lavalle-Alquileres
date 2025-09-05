7bd94d39db63c294be4d19e2c29969978cdec4ed
Gisella-Domizi <giselladomizi72@gmail.com>
2025-09-05 11:22:25 -0300

    Activity Perfil

--- app/src/main/java/com/giselladomizi/first/ui/main/PerfilActivity.kt
+++ app/src/main/java/com/giselladomizi/first/ui/main/PerfilActivity.kt
@@ -3,9 +3,13 @@ package com.giselladomizi.first.ui.main
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.giselladomizi.first.R
+import com.giselladomizi.first.data.local.databese.AppDatabase
+import kotlinx.coroutines.launch
 
 class PerfilActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,33 @@ class PerfilActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_perfil)
 
+        // Recuperar sesión
+        val prefs = getSharedPreferences("sesion", MODE_PRIVATE)
+        val idUsuario = prefs.getInt("id_usuario", -1)
+
+        if (idUsuario != -1) {
+            // Cargar datos del usuario desde la BD
+            val db = AppDatabase.getDatabase(this)
+
+            lifecycleScope.launch {
+                val perfil = db.perfilDAO().getPerfilByUserId(idUsuario)
+                if (perfil != null) {
+                    // Obtener referencias a todos los TextViews
+                    val pNombre = findViewById<TextView>(R.id.pNombre)
+                    val pApellido = findViewById<TextView>(R.id.pApellido)
+                    val pCorreo = findViewById<TextView>(R.id.pCorreo)
+                    val pTelefono = findViewById<TextView>(R.id.pTelefono)
+
+                    // Asignar los valores
+                    pNombre.text = "Nombre: ${perfil.nombre_p}"
+                    pApellido.text = "Apellido: ${perfil.apellido_p}"
+                    pCorreo.text = "Correo: ${perfil.correo_p}"
+                    pTelefono.text = "Teléfono: ${perfil.telefono_p}"
+                }
+            }
+        }
+
+        //Botones de Navegación
         val btnInicio: ImageButton = findViewById(R.id.btnInicio)
         val btnAddPublicacion: ImageButton = findViewById(R.id.btnAddPublicacion)
         val btnMisPublicabiones: ImageButton = findViewById(R.id.btnMisPublicabiones)
